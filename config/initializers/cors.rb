98cfb9f2b28535958456c604094caeba60a28933
alijm0806 <alijm1104@gmail.com>
2022-10-13 18:55:53 -0500

    add cors to allow for netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080"
+    origins "localhost:8080", "https://amazing-baklava-f9b4a2.netlify.app"
 
     resource "*",
       headers: :any,
